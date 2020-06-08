ef70d8c7ab406a15c1f067e6730bc6a21d6ddcb9
franciscocotto <lancegrs@gmail.com>
2020-06-07 21:04:37 -0600

    Formulario Add documentos

--- app/src/main/java/com/example/inventario/AddDocumentos.java
+++ app/src/main/java/com/example/inventario/AddDocumentos.java
@@ -1,24 +1,36 @@
 package com.example.inventario;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -27,78 +39,179 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.inventario.dialog.DatePickerFragment;
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
+
+public class AddDocumentos extends Fragment implements AdapterView.OnItemSelectedListener {
 
 
-public class AddDocumentos extends Fragment {
+
     public AddDocumentos() {
         // Required empty public constructor
     }
-    private Spinner spinner;
-    // array para listar las categorias
-    ArrayList<String> categorias;
-
-    // Url listar las categorias
-
+    private EditText etPlannedDate;
+    private Spinner spinnerFood;
+    // array list for spinner adapter
+    private ArrayList<Categorias> categoriesList;
+    // Url to get all categories
+    ProgressDialog pDialog;
+    private String URL_CATEGORIES = "https://inventario-pdm115.000webhostapp.com/getcategorias.php";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final  View view =  inflater.inflate(R.layout.fragment_add_documento, container, false);
+         View view =  inflater.inflate(R.layout.fragment_add_documento, container, false);
+        //Initializing Spinner
+        spinnerFood = (Spinner) view.findViewById(R.id.sp_categorias);
+        //Initializing the ArrayList
+        categoriesList = new ArrayList<Categorias>();
+        spinnerFood.setOnItemSelectedListener(this);
+       new GetCategories().execute();
 
-        categorias=new ArrayList<>();
-        final Spinner spinner = (Spinner) view.findViewById(R.id.sp_categorias);
-        listar();
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        etPlannedDate = (EditText) view.findViewById(R.id.etDate);
+        etPlannedDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String categorias =   spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
-                Toast.makeText(getActivity(),categorias,Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                // DO Nothing here
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.etDate:
+                        showDatePickerDialog();
+                        break;
+                }
             }
         });
-        return view;
+       return view;
+
+    }
+
+    /**
+     * Seleccionar Fecha
+     * */
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = twoDigits(day)  + " - " + twoDigits(month+1) + " - " + year;
+                etPlannedDate.setText(selectedDate);
+            }
+
+            private String twoDigits(int n) {
+                return (n<=9) ? ("0"+n) : String.valueOf(n);
+            }
+        });
+
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
     }
 
 
-    public void listar(){
-        String url = "https://inventario-pdm115.000webhostapp.com/listar-fruta.php";
-
-        RequestQueue requestQueue=Volley.newRequestQueue(getActivity());
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try{
-                            JSONObject jsonObject=new JSONObject(response);
+   /**
+     * Añadiendo datos a spinner categorias
+     * */
+    private void populateSpinner() {
+        List<String> lables = new ArrayList<String>();
 
 
-                            JSONArray jsonArray=jsonObject.getJSONArray("usuario");
-                            for(int i=0;i<jsonArray.length();i++){
-                                JSONObject jsonObject1=jsonArray.getJSONObject(i);
-                                String country=jsonObject1.getString("nombre");
-                                categorias.add(country);
-                            }
-                            spinner.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, categorias));
-                        }catch (JSONException e){e.printStackTrace();}
+
+        for (int i = 0; i < categoriesList.size(); i++) {
+            lables.add(categoriesList.get(i).getCategoria());
+        }
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_spinner_item, lables);
+
+        // Drop down layout style - list view with radio button
+        spinnerAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinnerFood.setAdapter(spinnerAdapter);
+    }
+
+
+    /**
+     * Async task to get all categories
+     * */
+    private class GetCategories extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(getContext());
+            pDialog.setMessage("Cargando Categorias");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            ServiceHandler jsonParser = new ServiceHandler();
+            String json = jsonParser.makeServiceCall(URL_CATEGORIES, ServiceHandler.GET);
+
+            Log.e("Response: ", "> " + json);
+
+            if (json != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    if (jsonObj != null) {
+                        JSONArray categories = jsonObj
+                                .getJSONArray("categories");
+
+                        for (int i = 0; i < categories.length(); i++) {
+                            JSONObject catObj = (JSONObject) categories.get(i);
+                            Categorias cat = new Categorias(catObj.getInt("id"),
+                                    catObj.getString("name"));
+                            categoriesList.add(cat);
+                        }
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.e("JSON Data", "No se recibe datos del servidor!");
             }
-        });
-        int socketTimeout = 30000;
-        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-        stringRequest.setRetryPolicy(policy);
-        requestQueue.add(stringRequest);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+               populateSpinner();
+        }
+
     }
 
 
-}
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(
+                getActivity().getApplicationContext(),
+                "Categoria " + parent.getItemAtPosition(position).toString() ,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+
+
+    }
+
 
 
