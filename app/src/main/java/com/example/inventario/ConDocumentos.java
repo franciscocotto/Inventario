0ae55fb0ecb2473ffe806e10eab6b115a6e826fd
vc17009 <66234929+vc17009@users.noreply.github.com>
2020-06-09 23:10:47 -0600

    subida de prueba

--- app/src/main/java/com/example/inventario/ConDocumentos.java
+++ app/src/main/java/com/example/inventario/ConDocumentos.java
@@ -18,7 +18,7 @@ public class ConDocumentos extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment prueba
        View v = inflater.inflate(R.layout.fragment_con_documentos, container, false);
 
        btnEjem = (Button) v.findViewById(R.id.btnEjemplo);
